27d7ea2e78b755f8bca2880d93d6a7b59c0f0a81
w0rm49 <hexonoize@gmail.com>
2017-03-02 23:52:04 +0300

    basic processing

--- svg-inliner/main.js
+++ svg-inliner/main.js
@@ -7,6 +7,8 @@
     //drop area
 
     var $dropZone = $('.drop-zone');
+    var $source = $('.source-svg');
+    var $target = $('.target-svg');
 
     $dropZone.on({
         'drag dragstart dragend dragover dragenter dragleave drop': function(e) {
@@ -21,14 +23,46 @@
         },
         'drop': function(e) {
             var droppedFiles = e.originalEvent.dataTransfer.files;
-            console.log(droppedFiles);
+
+            if (droppedFiles.length !== 1) {
+                return;
+            }
+
+            var reader = new FileReader();
+
+            reader.onloadend = function(evt) {
+                if (evt.target.readyState == FileReader.DONE) {
+                    $source.val(evt.target.result);
+                    process();
+                    // test result
+                }
+            };
+            reader.readAsText(droppedFiles[0]);
+
         }
     });
 
-    //process:
-    //  replace " with '
-    //  remove unnecessary attributes on svg tag
-    //  replace <, >, # with %3с, %3e, %23
-    // test result
+    function process() {
+        var src = $source.val();
+        var result = src;
 
+        //  remove unnecessary attributes on svg tag
+
+        result = replace(result, "\n", '');
+        result = replace(result, "\r", '');
+        result = replace(result, "\t", ' ');
+
+        result = replace(result, '"', "'");
+        result = replace(result, '<', '%3c');
+        result = replace(result, '<', '%3C');
+        result = replace(result, '>', '%3e');
+        result = replace(result, '>', '%3E');
+        result = replace(result, '#', '%23');
+
+        $target.html(result);
+    }
+
+    function replace(text, search, replace) {
+        return text.split(search).join(replace);
+    }
 })(jQuery);
